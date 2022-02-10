ae81623cdab97b51e1e31aca9324cba21e944025
bruce <bruce@unclesam.tw>
2022-02-10 18:10:36 +0800

    add thenXxx

--- src/CompletableFutureTest.java
+++ src/CompletableFutureTest.java
@@ -23,54 +23,55 @@ public class CompletableFutureTest {
     }
 
     private void initAsync() {
-        System.out.println("start");
+        printStart();
         CompletableFuture.runAsync(() -> {
             System.out.println("thread id=" + Thread.currentThread().getId());
         }, ES);
-        System.out.println("end");
-        // 包起來的 CompletableFuture.runAsync 是另外一個執行緒，通常執會執行 start 後 end，最後才是裡面的方法
+        printEnd();
+        // 包起來的 CompletableFuture.runAsync 是另外一個執行緒，如果沒有調用阻塞方法，通常執會執行 start 後 end，最後才是裡面的方法
     }
 
     private void initSupplyAsync() throws ExecutionException, InterruptedException {
-        System.out.println("start");
+        printStart();
         CompletableFuture<Long> rtn = CompletableFuture.supplyAsync(() -> {
             long id = Thread.currentThread().getId();
             System.out.println("thread id=" + id);
             return id;
         }, ES);
         System.out.println("rtn=" + rtn.get()); // get() 是阻塞方法，加了這行會等到前面都執行完才會往下執行
-        System.out.println("end");
+        printEnd();
+        // 包起來的 CompletableFuture.supplyAsync 是另外一個執行緒，如果沒有調用阻塞方法，通常執會執行 start 後 end，最後才是裡面的方法
     }
 
     private void whenCompleteTest() throws ExecutionException, InterruptedException {
         // whenComplete 為某個執行緒執行完之後才執行，只能知道有沒有異常，不能修改回傳值
 
-        System.out.println("start");
+        printStart();
         CompletableFuture<Long> rtn = CompletableFuture.supplyAsync(() -> {
             long id = Thread.currentThread().getId();
             System.out.println("thread id=" + id);
 //            int i = 1 / 0;
             return id;
-        }, ES).whenComplete((res, ex) -> { // whenComplete 使用上一個執行緒；whenCompleteAsync 用新的執行緒
+        }, ES).whenComplete((res, ex) -> { // whenComplete 使用 main 執行緒；whenCompleteAsync 用新的執行緒
             System.out.println("內部結果為=" + res);
             System.out.println("例外=" + ex);
         });
 
         // 如果有例外，以下程式碼不會執行
         System.out.println("外部結果為=" + rtn.get()); // get() 是阻塞方法，加了這行會等到前面都執行完才會往下執行
-        System.out.println("end");
+        printEnd();
     }
 
     private void exceptionallyTest() throws ExecutionException, InterruptedException {
         // 如果有例外就會執行 exceptionally，可修改回傳值
 
-        System.out.println("start");
+        printStart();
         CompletableFuture<Long> rtn = CompletableFuture.supplyAsync(() -> {
             long id = Thread.currentThread().getId();
             System.out.println("thread id=" + id);
 //            int i = 1 / 0;
             return id;
-        }, ES).whenCompleteAsync((res, ex) -> { // whenComplete 使用上一個執行緒；whenCompleteAsync 用新的執行緒
+        }, ES).whenCompleteAsync((res, ex) -> { // whenComplete 使用 main 執行緒；whenCompleteAsync 用新的執行緒
             System.out.println("內部結果為=" + res);
             System.out.println("例外=" + ex);
         }).exceptionally(t -> { // 沒有例外不會執行
@@ -79,24 +80,33 @@ public class CompletableFutureTest {
         });
         // 因為有 exceptionally，如果有例外，以下程式碼還是會執行
         System.out.println("外部結果為=" + rtn.get()); // get() 是阻塞方法，加了這行會等到前面都執行完才會往下執行
-        System.out.println("end");
+        printEnd();
     }
 
     private void handleTest() throws ExecutionException, InterruptedException {
         // handle 是 whenComplete + exceptionally，不管有沒有例外，都可修改回傳值
 
-        System.out.println("start");
+        printStart();
         CompletableFuture<Long> rtn = CompletableFuture.supplyAsync(() -> {
             long id = Thread.currentThread().getId();
             System.out.println("thread id=" + id);
             int i = 1 / 0;
             return id;
-        }, ES).handle((res, ex) -> { // handle 使用上一個執行緒；handleAsync 用新的執行緒
+        }, ES).handleAsync((res, ex) -> { // handle 使用 main 執行緒；handleAsync 用新的執行緒
+            System.out.println("handle thread id=" + Thread.currentThread().getId());
             System.out.println("內部結果為=" + res);
             System.out.println("例外=" + ex);
             return 999L;
         });
         System.out.println("外部結果為=" + rtn.get()); // get() 是阻塞方法，加了這行會等到前面都執行完才會往下執行
+        printEnd();
+    }
+
+    private void printStart() {
+        System.out.println("start");
+    }
+
+    private void printEnd() {
         System.out.println("end");
     }
 }
--- src/ThenXxxTest.java
+++ src/ThenXxxTest.java
@@ -1,31 +1,176 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ThenXxxTest {
     private static final ExecutorService ES = Executors.newFixedThreadPool(10);
 
     /**
+     * 一個執行緒完成後，接著往下執行
      * thenRun 無參無回傳值，直接執行，不需要上一個執行緒的結果
      * thenAccept 有參無回傳值，參數為上一個執行緒的結果
      * thenApply 有參有回傳值，可讓下一個執行緒知道返回的結果
-     * 以上都有相應的 xxxAsync 方法
+     * 以上都有相應的 xxxAsync 方法，然後再分成有沒有 Executor 參數
      */
-    public static void main(String[] args) {
+    /**
+     * 兩個執行緒完成才能往下執行
+     * CompletableFuture 有實現 CompletionStage，所以這個參數傳第二個執行緒
+     * 和上面三個方法一樣，參數都是上兩個執行緒回傳的結果，回傳值是自己回傳的結果
+     * <p>
+     * runAfterBoth(CompletionStage, Runnable)，無參無回傳值，無法取得兩個執行緒的結果
+     * thenAcceptBoth(CompletionStage, BiConsumer)，有兩個參數，無回傳值，可取得兩個執行緒的結果
+     * thenCombine(CompletionStage, BiFunction) 有參有回傳值，可取得兩個執行緒的結果，也可回傳自己的結果
+     * 以上都有相應的 xxxAsync 方法，然後再分成有沒有 Executor 參數
+     */
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ThenXxxTest tx = new ThenXxxTest();
-        tx.tenRunTest();
+//        tx.thenRunTest();
+//        tx.thenAcceptTest();
+//        tx.thenApplyTest();
+//        tx.thenApplyAcceptRunTest();
+//        tx.runAfterBothTest();
+//        tx.thenAcceptBothTest();
+        tx.thenCombineTest();
 
         if (!ES.isShutdown()) ES.shutdown();
     }
 
-    private void tenRunTest() {
-
+    private void thenRunTest() {
+        printStart();
+        CompletableFuture.supplyAsync(() -> {
+            long id = Thread.currentThread().getId();
+            System.out.println("thread id=" + id);
+            return id;
+        }, ES).thenRun(() -> {
+            System.out.println("thenRun thread id=" + Thread.currentThread().getId());
+            sleep();
+        });
+        // 以上都執行完才會往下執行
+        printEnd();
     }
 
-    private void tenAcceptTest() {
-
+    private void thenAcceptTest() {
+        printStart();
+        CompletableFuture.supplyAsync(() -> {
+            long id = Thread.currentThread().getId();
+            System.out.println("thread id=" + id);
+            return id;
+        }, ES).thenAccept(lastResult -> {
+            System.out.println("lastResult=" + lastResult);
+            System.out.println("thenAccept thread id=" + Thread.currentThread().getId());
+            sleep();
+        });
+        // 以上都執行完才會往下執行
+        printEnd();
     }
 
-    private void tenApplyTest() {
+    private void thenApplyTest() throws ExecutionException, InterruptedException {
+        printStart();
+        CompletableFuture<Integer> result = CompletableFuture.supplyAsync(() -> {
+            long id = Thread.currentThread().getId();
+            System.out.println("thread id=" + id);
+            return id;
+        }, ES).thenApply(lastResult -> {
+            System.out.println("lastResult=" + lastResult);
+            System.out.println("thenApply thread id=" + Thread.currentThread().getId());
+            sleep();
+            return 333;
+        });
+        // 以上都執行完才會往下執行
+        System.out.println(result.get());
+        printEnd();
     }
 
+    private void thenApplyAcceptRunTest() {
+        printStart();
+        CompletableFuture.supplyAsync(() -> {
+            long id = Thread.currentThread().getId();
+            System.out.println("thread id=" + id);
+            return id;
+        }, ES).thenApply(lastResult -> {
+            System.out.println("lastResult1=" + lastResult);
+            System.out.println("thenApply thread id=" + Thread.currentThread().getId());
+            sleep();
+            return 333;
+        }).thenAccept(lastResult -> {
+            System.out.println("lastResult2=" + lastResult);
+            System.out.println("thenAccept thread id=" + Thread.currentThread().getId());
+            sleep();
+        }).thenRun(() -> {
+            System.out.println("thenRun thread id=" + Thread.currentThread().getId());
+            sleep();
+        });
+        // 每個 thenXxx 方法都做完才會到下一個 thenXxx 方法，以上都執行完才會往下執行
+        printEnd();
+    }
+
+    private void runAfterBothTest() {
+        printStart();
+
+        getThread1().runAfterBoth(getThread2(), () -> {
+            System.out.println("yeah");
+            sleep();
+        });
+
+        // 以上都執行完才會往下執行
+        printEnd();
+    }
+
+    private void thenAcceptBothTest() {
+        printStart();
+
+        getThread1().thenAcceptBoth(getThread2(), (t1, t2) -> {
+            System.out.println("t1=" + t1);
+            System.out.println("t2=" + t2);
+            sleep();
+        });
+
+        // 以上都執行完才會往下執行
+        printEnd();
+    }
+
+    private void thenCombineTest() throws ExecutionException, InterruptedException {
+        printStart();
+
+        CompletableFuture<String> result = getThread1().thenCombine(getThread2(), (t1, t2) -> {
+            System.out.println("t1=" + t1);
+            System.out.println("t2=" + t2);
+            sleep();
+            return t1 + t2;
+        });
+
+        // 以上都執行完才會往下執行
+        System.out.println(result.get());
+        printEnd();
+    }
+
+    private void printStart() {
+        System.out.println("start");
+    }
+
+    private void printEnd() {
+        System.out.println("end");
+    }
+
+    private void sleep() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CompletableFuture<Integer> getThread1() {
+        return CompletableFuture.supplyAsync(() -> {
+            int i = 123;
+            System.out.println("i=" + i);
+            return i;
+        }, ES);
+    }
+
+    private CompletableFuture<String> getThread2() {
+        return CompletableFuture.supplyAsync(() -> {
+            String str = "xxx";
+            System.out.println("str=" + str);
+            return str;
+        }, ES);
+    }
 }
